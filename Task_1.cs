6732196903853d0f73c59f5536b22b422978fe38
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2022-09-05 09:50:08 +0300

    Task_1

---
+++ Task_1.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вторая цифра числа {n}: {n / 10 % 10}");
+
+
+Console.Write("Введи трёхзначное число: ");
+string stroka = Console.ReadLine();
+Console.WriteLine($"Вторая цифра числа {stroka}: {stroka[1]}");
