24e7b7bb8d06dbd52543bc212e9d8b3072048d81
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2022-09-05 09:50:33 +0300

    Task_2

---
+++ Task_2.cs
@@ -0,0 +1,17 @@
+﻿void function(string n)
+{
+    if (n.Length > 2)
+    {
+        Console.Write("Третья цифра: " + n[2]);
+    }
+    else
+    {
+        Console.Write("Третьей цифры нет!");
+    }
+
+}
+
+Console.Write("Введи число: ");
+string n = Console.ReadLine();
+function(n);
+
