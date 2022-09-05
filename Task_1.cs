49ab573dd025f185d0e295c8b92efd5be692dee2
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2022-09-05 09:51:35 +0300

    Update Task_1.cs

--- Task_1.cs
+++ Task_1.cs
@@ -2,7 +2,7 @@
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Вторая цифра числа {n}: {n / 10 % 10}");
 
-
+//Второй вариант 
 Console.Write("Введи трёхзначное число: ");
 string stroka = Console.ReadLine();
 Console.WriteLine($"Вторая цифра числа {stroka}: {stroka[1]}");
